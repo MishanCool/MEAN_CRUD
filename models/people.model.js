a45240eab793523d226dd48540d09614585210cd
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-15 23:52:52 +0530

    Create collections in database

---
+++ models/people.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+ 
+const peopleRegister = new Schema({    //create peopleRegister object
+
+  fullName : {
+      type : String
+  },
+  email : {
+      type : String
+  },
+  mobile : {
+      type : String
+  },
+  city : {
+      type : String
+  },
+
+});
+
+mongoose.model('people', peopleRegister);  //const MyModel = mongoose.model('ModelName', mySchema);
