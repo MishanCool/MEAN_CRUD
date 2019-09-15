2ef22557cc47e56b492bef0777fac7acc4192ff7
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-15 18:30:14 +0530

    Create conection with mongodb database

---
+++ models/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/moder', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true},
+      (err) => {
+          if(!err) {console.log('MongoDB conection success.')}
+          else {console.log('MongoDB not conected. err : '+err)}
+  });
