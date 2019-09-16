8a6ad9fd2208607678617154f59705fff1ded072
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-16 13:14:18 +0530

    Configure routing

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require('./models/db');
 
 const express = require('express');
 
+const peopleController = require('./controllers/people.controller');
+
 var app = express();
 
 app.listen(3004 , () => {
     console.log('Express server started at port : 3004');
 });
+
+app.use('/people', peopleController); //set url to peoplecontroller
