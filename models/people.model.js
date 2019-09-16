7c0c59b68db803a4e9e65231b9130b08fa91c4e1
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-16 13:17:57 +0530

    ## Navigate new route people ##

--- models/people.model.js
+++ models/people.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
  
-const peopleRegister = new Schema({    //create peopleRegister object
+const peopleRegister = new Schema({  //create peopleRegister object
 
   fullName : {
       type : String
