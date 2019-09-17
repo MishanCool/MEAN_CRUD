2ef72a01f55e6d65ab7e013ad79bd3b24d56e6bd
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-17 23:43:21 +0530

    ## Create add and edit interface ##

--- controllers/people.controller.js
+++ controllers/people.controller.js
@@ -7,9 +7,11 @@ var router = express.Router();
                 //request handler function
 router.get('/', (req,res) => {
     //res.json('Sample text')
+    
     res.render("people/addOrEdit", {
         viewTitle: "Insert people" //addOrEdit file
     });
+
 });
 
 module.exports = router; //exports router object
