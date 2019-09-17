8459c95813c4038d02fb9a90e3c390905ee12072
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-17 23:25:10 +0530

    Require path , express-handlebars and Set hbs file extention

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ require('./models/db');
 
 const express = require('express');
 
+const path = require('path');
+const exphanbars = require('express-handlebars');
+
 const peopleController = require('./controllers/people.controller');
 
 var app = express();
+
+app.set('views' , path.join(__dirname , '/views/'));
+app.engine('hbs', exphanbars({ extname: 'hbs' , defaultLayout: 'mainLayout' , layoutsDir: __dirname + '/views/layouts/'}));
+app.set('view engine', 'hbs');
 
 app.listen(3004 , () => {
     console.log('Express server started at port : 3004');
